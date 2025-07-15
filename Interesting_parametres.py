6dc721ae494db5d08062aa39d4c8e680aa6dfc93
matanou <74264813+matanou@users.noreply.github.com>
2025-07-15 16:38:47 +0100

    Create Interesting_parametres.py

---
+++ Interesting_parametres.py
@@ -0,0 +1,16 @@
+"""
+By 'interesting' I mean any set of parametres that doesn't cause the population or the resource to diverges. To find most of them I am currently using a program to checks if the absolute value of the "final value" of both the resources and the population is below 100 for a range of parametres. I'll add them when Im satisfied with the set of parametres I get
+"""
+
+
+init_pop = 5
+init_res = 11
+init_growth = 0.01
+init_consumption = 0.2422
+init_replenishment = 0.989
+dt = 0.01
+simulation_time = 2000
+
+
+
+
